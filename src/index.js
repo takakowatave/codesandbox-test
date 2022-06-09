0d7440efbb25ecabcfa53aec5c3513f8861dfd8d
takakowatave <103505170+takakowatave@users.noreply.github.com>
2022-06-09 09:05:36 +0900

    こーしん

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+/**
+ * const,letの変数宣言
+ */
+// var val1 = "var変数";
 
-import App from "./App";
+// // var変数は上書き可能
+// let val2 = "let変数";
+// console.log(val2);
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// var vall = "var変数を再宣言";
+// console.log(vall);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// let val2 = "let変数";
+// console.log(val2);
 
-console.log("aaaaaaa");
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// val3 = "const変数を上書き"；
+
+// const val4 = {
+//   name: "たあ",
+//   age: 28
+// };
+// val4.name = "t";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+console.log(val5);
